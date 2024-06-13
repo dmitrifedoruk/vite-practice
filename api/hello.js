5cdb6744afc5f2790565ffed1cacabe2dcc18492
D F <dmitrifedoruk@gmail.com>
2024-06-13 13:03:33 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,8 +1,10 @@
 export async function GET(request) {
 
-    const api_string = process.env.WORD;
+    const api_string = {
+        data: process.env.WORD
+    }
 
     const myOptions = { status: 200, statusText: "SuperSmashingGreat!"};
 
-    return new Response(api_string, myOptions);
+    return new Response(JSON.stringify(api_string), myOptions);
 }
