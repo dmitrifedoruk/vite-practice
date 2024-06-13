ce44cb1c764985f7601ca1d2f5e8cd15dd81cace
D F <dmitrifedoruk@gmail.com>
2024-06-13 08:50:35 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,4 +1,5 @@
-export async function GET(request) {
-
-    return new Response(`https://api.dictionaryapi.dev/api/v2/entries/en/${process.env.WORD}`);
+module.exports = (req, res) => {
+    res.json({
+        hola: 'mundo'
+    })
 }
