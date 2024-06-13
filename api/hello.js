b9c9408276605074d0c68cf3d5106638e16bfda4
D F <dmitrifedoruk@gmail.com>
2024-06-13 08:48:30 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,15 +1,4 @@
 export async function GET(request) {
 
-
-    const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${process.env.WORD}`);
-
-    let data = "";
-
-    data = await response;
-
-    const definition = data[0]["meanings"][0]["definitions"][0]["definition"].toString();
-
-
-
-    return new Response(definition);
+    return new Response(`https://api.dictionaryapi.dev/api/v2/entries/en/${process.env.WORD}`);
 }
