06fa668f5d64ae43db07f3dde243eb4b6cb68cad
D F <dmitrifedoruk@gmail.com>
2024-06-18 15:50:35 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,12 +1,14 @@
-export function GET(request) {
+export async function GET(request) {
 
     const api_string = {
         data: process.env.WORD
     }
 
-    // const myOptions = { status: 200, statusText: "SuperSmashingGreat!"};
+    const myOptions = { status: 200, statusText: "SuperSmashingGreat!"};
 
-    const response = fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${api_string}`);
+    const response = "kalamazoo";
+
+    // const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/${api_string}`);
 
     let data = "";
 
