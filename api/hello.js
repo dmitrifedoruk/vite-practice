452a9ca1e148ed045870e322ddd5a27f2e9ac9f6
D F <dmitrifedoruk@gmail.com>
2024-06-18 20:51:20 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,5 @@
 export async function GET(request) {
 
-    import fetch from 'node-fetch';
-
     const response = await fetch(`https://api.dictionaryapi.dev/api/v2/entries/en/hamburger`);
     let data = "";
     data = await response.json();
