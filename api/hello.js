3637f554210c4aed31e257e944a25afd790a8dfe
D F <dmitrifedoruk@gmail.com>
2024-06-19 16:11:53 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,3 +1,38 @@
-module.exports = (request, response) => {
-    response.json({ property: 'value' })
+export async function GET(request) {
+
+    const lat = 39.5839498;
+    const lon = -77.045579;
+
+    // const api_url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`;
+    //
+    // const response = await fetch(api_url);
+
+    // let data = await response.json();
+
+    // const data = {
+    //     latitude: lat,
+    //     longitude: lon
+    // }
+
+    const data = request.query;
+
+    let response = "";
+
+    if(data.empty()){
+        response= "tartar sauce"
+    }
+    else{
+        response = "peanuts"
+    }
+
+
+
+    // const myObject = {
+    //     data : `https://api.dictionaryapi.dev/api/v2/entries/en/${process.env.WORD}`
+    // }
+
+
+
+
+    return new Response(response);
 }
