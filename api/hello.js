d6b17284123c481c881a3344c2d2ddaef356b8e2
D F <dmitrifedoruk@gmail.com>
2024-06-19 15:42:24 -0400

    testing api

--- api/hello.js
+++ api/hello.js
@@ -1,7 +1,7 @@
 export async function GET(request) {
 
-    // const lat = 39.5839498;
-    // const lon = -77.045579;
+    const lat = 39.5839498;
+    const lon = -77.045579;
 
     // const api_url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&appid=${process.env.API_KEY}`;
     //
