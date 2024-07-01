dd941eef611dfb54ad5fd7f5b5f641342e0bb606
D F <dmitrifedoruk@gmail.com>
2024-07-01 15:13:53 -0400

    testing function

--- api/add-row.js
+++ api/add-row.js
@@ -6,7 +6,7 @@ export default async function handler(request, response) {
         const banana = request.query.banana;
         const roast = request.query.roast;
 
-        if (!size || !roast) throw new Error('Info required');
+        if (!banana || !roast) throw new Error('Info required');
         await sql`INSERT INTO Coffee (Banana, Roast) VALUES (${banana}, ${roast});`;
     } catch (error) {
         return response.status(500).json({ error });
