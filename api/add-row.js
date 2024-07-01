d311049ec3071cafd5fe9a28a786d4b9f838910d
D F <dmitrifedoruk@gmail.com>
2024-07-01 15:08:04 -0400

    testing function

--- api/add-row.js
+++ api/add-row.js
@@ -3,11 +3,11 @@ import { sql } from '@vercel/postgres';
 export default async function handler(request, response) {
     try {
 
-        const size = request.query.size;
+        const banana = request.query.banana;
         const roast = request.query.roast;
 
         if (!size || !roast) throw new Error('Info required');
-        await sql`INSERT INTO Coffee (Size, Roast) VALUES (${size}, ${roast});`;
+        await sql`INSERT INTO Coffee (Banana, Roast) VALUES (${banana}, ${roast});`;
     } catch (error) {
         return response.status(500).json({ error });
     }
